cc198f27523cc963c58f42d897944148088f4602
Matheus Goetz Oyarzabal <matheusgoetz@gmail.com>
2022-03-04 14:45:25 -0300

    Projeto Filmes

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './style.css';
+import Routes from './routes';
+
+export default function App() {
+  return (
+    <div className='app'>
+      <Routes/>
+    </div>
+  );
+}
+
+
+
+
---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+import Header from './components/Header';
+
+import Home from './pages/Home';
+
+
+const Routes = () => {
+    return(
+        <BrowserRouter>
+            <Header/>
+            <Switch>
+                <Route exact path='/' component={Home} />
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Routes;
