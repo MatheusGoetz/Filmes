526295f990caf8fbd63fa38a20155d76c6c4135b
Matheus Goetz Oyarzabal <matheusgoetz@gmail.com>
2022-04-02 09:23:53 -0300

    add build e fazendo deploy

--- src/pages/Erro/index.js
+++ src/pages/Erro/index.js
@@ -6,7 +6,7 @@ import {Link} from 'react-router-dom'
 export default function Erro(){
     return(
         <div className='not-found'>
-            <h1>404!</h1>
+            <h1>erro 404!</h1>
             <h2>Pagina não encontrada!</h2>
             <Link to="/">Veja todos os filmes</Link>
         </div>
